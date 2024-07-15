5359e6a0453a9dbde81dea170ae1b194e9896eb8
MIDHUN KUMAR N <130952023+midhunkumarn@users.noreply.github.com>
2024-07-15 16:26:05 +0530

    sidebar

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,10 +1,27 @@
 import React from "react"
 import './Dashboard.css';
+import { SidebarData } from './SidebarData';
+
+
+
 export default function Dashboard(){
     return (
-        <div className="sidebar">
-            
-        </div>
+        <div className="Sidebar">
+            <ul className="SidebarList">
+                {SidebarData.map((val, key) =>{
+                    return(
+                        <li key={key} 
+                        className="row"
+                        onClick={() => {window.location.pathname = val.link}}>
 
+                            <div id="icon">{val.icon}</div>
+                            <div id="title">
+                                {val.title}
+                            </div>
+                        </li>
+                    );      
+                })}
+            </ul>
+        </div>
     );
 }
--- src/component/SidebarData.js
+++ src/component/SidebarData.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import DetailsIcon from '@mui/icons-material/Details';
+import HistoryToggleOffIcon from '@mui/icons-material/HistoryToggleOff';
 
 export const SidebarData = [
     {
        title:"Home",
-       icon:<Icon/>,
+       icon:<HomeIcon/>,
        link:"/home"
-      }
+      },
+      {
+        title:"Details",
+        icon:<DetailsIcon/>,
+        link:"/details"
+       },
+       {
+        title:"Excemption",
+        icon:<HistoryToggleOffIcon/>,
+        link:"/home"
+       },
 ]
 
